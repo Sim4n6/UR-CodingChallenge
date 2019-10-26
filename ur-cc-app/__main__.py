bb6e97621d0d1b3ccc25fcee3504385b72648066
Sim4n6 <sim4n6@gmail.com>
2019-10-26 20:08:37 +0100

    Use of Flask-bootstrap extension

--- ur-cc-app/__main__.py
+++ ur-cc-app/__main__.py
@@ -1,9 +1,14 @@
 from .landing import landing_routes
 
 from flask import Flask, render_template, make_response, request, jsonify, Blueprint
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
+Bootstrap(app)
+
+# register bluprints
 app.register_blueprint(landing_routes.landing_bp)
+
 
 # load env vars
 from dotenv import load_dotenv
