4433f330746572eb64f3c4fc27e664b1f0ab4d68
Sim4n6 <sim4n6@gmail.com>
2019-11-03 13:27:35 +0100

    Specify ENV environment variable to tell FLask and extension the context environment : Production or Development

--- ur_cc_app/config.py
+++ ur_cc_app/config.py
@@ -13,12 +13,14 @@ class ProdConfig(Config):
     TESTING = False
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SECRET_KEY = os.getenv("SECRET_KEY")
+    ENV = "production"
 
 
 class DevConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join( basedir , 'db.sqlite3' )}"
     SECRET_KEY = "#l9@)_DF6451FS165"
+    ENV = "development"
 
 
 config_choices = {"Production": ProdConfig, "Development": DevConfig}
