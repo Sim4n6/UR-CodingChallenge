a02f8c5dcf448427629c0e0b4e887a430d8f2bf3
Sim4n6 <sim4n6@gmail.com>
2019-11-08 13:51:23 +0100

    write 03 functions to hide/mute card/btn and send async request to api side of the app

---
+++ ur_cc_app/static/script.js
@@ -0,0 +1,44 @@
+
+
+function dislike(dislike_btn) {
+    // mute btn like 
+    dislike_btn.disabled = true;
+    dislike_btn.nextSibling.disabled = false;
+
+    // get id of current shop
+    let shopId = dislike_btn.parentNode.parentNode.parentNode.id;
+
+    // send async post the shop to the api side 
+    const xhr = new XMLHttpRequest();
+    xhr.open('DELETE', '/api/v1/preferred_shops/'+ shopId);
+    xhr.send()
+}
+
+function like(like_btn) {
+    
+    // mute btn like 
+    like_btn.disabled = true;
+    like_btn.previousSibling.disabled = false;
+
+    // get id of current shop
+    let shopId = like_btn.parentNode.parentNode.parentNode.id;
+
+    // send async post the shop to the api side 
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/api/v1/preferred_shops/'+shopId);
+    xhr.send()
+}
+
+function removePreferred(remove_btn) {
+    // hide div of dislike_btn
+    remove_btn.parentNode.parentNode.parentNode.style.display = 'none';
+
+    // get id of current shop
+    let shopId = remove_btn.parentNode.parentNode.parentNode.id;
+
+    // send async delete to the api side 
+    const xhr = new XMLHttpRequest();
+    xhr.open('DELETE', '/api/v1/preferred_shops/'+ shopId);
+    xhr.send()
+
+}
