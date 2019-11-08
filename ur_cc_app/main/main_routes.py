961221a10aab60f24fb266468336284e2209c2ce
Sim4n6 <sim4n6@gmail.com>
2019-11-08 18:58:54 +0100

    nearby shows only the none preferred shops and no requests btw blueprints

--- ur_cc_app/main/main_routes.py
+++ ur_cc_app/main/main_routes.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, request, abort
 from flask import render_template
 
-from requests.exceptions import RequestException
-import requests
+from ..api import api_routes
 
 # Blueprint Configuration
 main_bp = Blueprint(
@@ -14,45 +13,39 @@ main_bp = Blueprint(
 )
 
 
+@main_bp.route("/")
+@main_bp.route("/index")
+def index():
+    """ Index view function """
+    abort(404)
+
+
 @main_bp.route("/nearby", methods=["GET"])
 def nearby():
     """Nearby shops view function."""
 
-    try:
-        all_shops = []
-        # FIXME prepare an url independant from localhost
-        payload = {"limit": 5, "sortByDistance": 1}
-        r = requests.get("http://localhost:5000/api/v1/shops", params=payload)
-        if r.status_code == 200:
-            all_shops = r.json()
-        else:
-            abort(r.status_code, r.json().get("description"))
+    # get all shops
+    response = api_routes.listAllShops(limit=10, sortByDistance=1)
+    all_shops = response[0].get_json()
 
-    except RequestException as e:
-        abort(500, str(e))
-    else:
-        return render_template("nearby.html", title="Nearby shops", all_shops=all_shops)
+    # get all preferred shops
+    response = api_routes.listAllPreferredShops(sortByDistance=1)
+    all_preferred_shops = response[0].get_json()
+
+    # keep only those shops that are not in preferred shops
+    the_shops = [shop for shop in all_shops if shop not in all_preferred_shops]
+
+    return render_template("nearby.html", title="Nearby shops", all_shops=the_shops)
 
 
 @main_bp.route("/preferred", methods=["GET"])
 def preferred():
     """Preferred shops view function."""
 
-    try:
-        all_preferred_shops = []
-        # FIXME prepare an url independant from localhost
-        payload = {"limit": 5, "sortByDistance": 1}
-        r = requests.get("http://localhost:5000/api/v1/preferred_shops", params=payload)
-        if r.status_code == 200:
-            all_preferred_shops = r.json()
-        else:
-            abort(r.status_code, r.json().get("description"))
-
-    except RequestException as e:
-        abort(500, str(e))
-    else:
-        return render_template(
-            "preferred.html",
-            title="Preferred shops",
-            all_preferred_shops=all_preferred_shops,
-        )
+    response = api_routes.listAllPreferredShops(sortByDistance=1)
+    all_preferred_shops = response[0].get_json()
+    return render_template(
+        "preferred.html",
+        title="Preferred shops",
+        all_preferred_shops=all_preferred_shops,
+    )
